67f175751fd454f5d84c4205e6ceddc1084973c4
veniracitra <100218385+veniracitra@users.noreply.github.com>
2022-04-26 19:54:30 +0700

    Create biodataAct.php

---
+++ biodataAct.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>Biodata</title>
+</head>
+<body>
+<center><h1 >BIODATA ANDA</h1></center>
+</body>
+
+<?php
+echo " Nama : ".$_POST['nama']."<br><br>";
+echo " NPM : ".$_POST['npm']."<br><br>";
+echo " Alamat : ".$_POST['alamat']."<br><br>";
+echo " Jenis Kelamin : ".$_POST['jenis_kelamin']."<br><br>";
+echo " Tanggal Lahir : ".$_POST['tanggal']."<br><br>";
+echo " Hobby : ".$_POST['hobby']."<br>";
+
+?>
